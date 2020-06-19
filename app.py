b496e384bb418872a92844430bfcbe52e49df812
vijaya2497 <sweetyvijaya06@gmail.com>
2020-06-19 02:03:13 -0700

    adding modified app.py & templates folder

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
 from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session, abort
+
+
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello():
-    return "Hello World!"
+def home():
+  return "hello"
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return "login sucessful"
+    return render_template('login.html', error=error)
 
 
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello {}!".format(name)
-
-if __name__ == '__main__':
-    app.run(port=5000)
+if __name__ == "__main__":
+  app.run(port=2000)
 
